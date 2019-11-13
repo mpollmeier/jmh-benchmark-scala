4b0f90e8c8c55eff89c1dfcdf08f269acf3675a2
Michael Pollmeier <michael@michaelpollmeier.com>
2019-11-13 21:57:51 +1300

    WIP

--- src/main/scala/jmhtest/visitor/VisitorJmh.scala
+++ src/main/scala/jmhtest/visitor/VisitorJmh.scala
@@ -2,65 +2,91 @@ package jmhtest.visitor
 
 import org.openjdk.jmh.annotations._
 
-// @Warmup(iterations = 20)
-// @Measurement(iterations = 100)
-@Warmup(iterations = 2)
-@Measurement(iterations = 10)
-@State(Scope.Benchmark)
-class VisitorJmh {
-  val Iterations = 1000
-
-  @Benchmark
-  @BenchmarkMode(Array(Mode.Throughput))
-  @Fork(value = 1)
-  def visitor1: Unit = {
-    val iter = infiniteNodeIterator(new N1)
-    1.to(Iterations).foreach(_ =>
-      iter.next.accept(Visitor1)
-    )
-  }
-
-  @Benchmark
-  @BenchmarkMode(Array(Mode.Throughput))
-  @Fork(value = 1)
-  def visitor2a: Unit = {
-    val iter = infiniteNodeIterator(new N1, new N2)
-    1.to(Iterations).foreach(_ =>
-      iter.next.accept(Visitor2)
-    )
-  }
-
-  @Benchmark
-  @BenchmarkMode(Array(Mode.Throughput))
-  @Fork(value = 1)
-  def visitor2b: Unit = {
-    val iter = infiniteNodeIterator(new N10, new N11)
-    1.to(Iterations).foreach(_ =>
-      iter.next.accept(Visitor2)
-    )
-  }
-
-  @Benchmark
-  @BenchmarkMode(Array(Mode.Throughput))
-  @Fork(value = 1)
-  def visitor3: Unit = {
-    val iter = infiniteNodeIterator(new N1, new N2, new N3)
-    1.to(Iterations).foreach(_ =>
-      iter.next.accept(Visitor3)
-    )
-  }
+object VisitorJmh {
+  val iter2 = infiniteNodeIterator(new N1, new N2)
+  val iter3 = infiniteNodeIterator(new N4, new N5, new N6)
 
   def infiniteNodeIterator(nodes: Node*) = new java.util.Iterator[Node] {
-    val nodes: Array[Node] = nodes.toArray
-    val nodeCount = nodes.size
+    val nodesArr: Array[Node] = nodes.toArray
+    val nodeCount = nodesArr.size
 
     var i = 0
     def hasNext(): Boolean = true
     def next(): Node = {
-      val node = nodes(i)
+      val node = nodesArr(i)
       i += 1
       if (i == nodeCount) i = 0
       node
     }
   }
+}
+
+// @Warmup(iterations = 20)
+// @Measurement(iterations = 100)
+// @Warmup(iterations = 2)
+// @Measurement(iterations = 10)
+@Warmup(iterations = 1)
+@Measurement(iterations = 2)
+@State(Scope.Benchmark)
+class VisitorJmh {
+  import VisitorJmh._
+  val Iterations = 1000
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.Throughput))
+  @Fork(value = 1)
+  // @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+LogCompilation", "-XX:+PrintAssembly" ,"-XX:+PrintInterpreter", "-XX:+PrintNMethods", "-XX:+PrintNativeNMethods", "-XX:+PrintSignatureHandlers", "-XX:+PrintAdapterHandlers", "-XX:+PrintStubCode", "-XX:+PrintCompilation", "-XX:+PrintInlining", "-XX:CompileThreshold=1000"))
+  def visitor2: Unit = {
+    var i = 0
+    while (i < Iterations) {
+      i += 1
+      iter2.next.accept(Visitor2)
+    }
+    println(s"visitor2: $i")
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.Throughput))
+  @Fork(value = 1)
+  // @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+LogCompilation", "-XX:+PrintAssembly" ,"-XX:+PrintInterpreter", "-XX:+PrintNMethods", "-XX:+PrintNativeNMethods", "-XX:+PrintSignatureHandlers", "-XX:+PrintAdapterHandlers", "-XX:+PrintStubCode", "-XX:+PrintCompilation", "-XX:+PrintInlining", "-XX:CompileThreshold=1000"))
+  def visitor3: Unit = {
+    var i = 0
+    while (i < Iterations) {
+      i += 1
+      iter3.next.accept(Visitor3)
+    }
+    println(s"visitor3: $i")
+  }
+
+
+  // @Benchmark
+  // @BenchmarkMode(Array(Mode.Throughput))
+  // @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+LogCompilation", "-XX:+PrintAssembly" ,"-XX:+PrintInterpreter", "-XX:+PrintNMethods", "-XX:+PrintNativeNMethods", "-XX:+PrintSignatureHandlers", "-XX:+PrintAdapterHandlers", "-XX:+PrintStubCode", "-XX:+PrintCompilation", "-XX:+PrintInlining"))
+  // def visitor2a: Unit = {
+  //   val iter = infiniteNodeIterator(new N10, new N11)
+  //   1.to(Iterations).foreach(_ =>
+  //     iter.next.accept(Visitor2a)
+  //   )
+  // }
+
+  // @Benchmark
+  // @BenchmarkMode(Array(Mode.Throughput))
+  // @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+LogCompilation", "-XX:+PrintAssembly" ,"-XX:+PrintInterpreter", "-XX:+PrintNMethods", "-XX:+PrintNativeNMethods", "-XX:+PrintSignatureHandlers", "-XX:+PrintAdapterHandlers", "-XX:+PrintStubCode", "-XX:+PrintCompilation", "-XX:+PrintInlining"))
+  // def visitor2b: Unit = {
+  //   val iter = infiniteNodeIterator(new N1, new N11)
+  //   1.to(Iterations).foreach(_ =>
+  //     iter.next.accept(Visitor2a)
+  //   )
+  // }
+
+  // @Benchmark
+  // @BenchmarkMode(Array(Mode.Throughput))
+  // @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+LogCompilation", "-XX:+PrintAssembly" ,"-XX:+PrintInterpreter", "-XX:+PrintNMethods", "-XX:+PrintNativeNMethods", "-XX:+PrintSignatureHandlers", "-XX:+PrintAdapterHandlers", "-XX:+PrintStubCode", "-XX:+PrintCompilation", "-XX:+PrintInlining"))
+  // def visitor2c: Unit = {
+  //   val iter = infiniteNodeIterator(new N1, new N10, new N11)
+  //   1.to(Iterations).foreach(_ =>
+  //     iter.next.accept(Visitor2a)
+  //   )
+  // }
+
 }
--- src/main/scala/jmhtest/visitor/VisitorVsMatch.scala
+++ src/main/scala/jmhtest/visitor/VisitorVsMatch.scala
@@ -4,174 +4,153 @@ import org.openjdk.jmh.annotations._
 
 // @Warmup(iterations = 20)
 // @Measurement(iterations = 100)
-@Warmup(iterations = 2)
-@Measurement(iterations = 10)
-@State(Scope.Benchmark)
-class VisitorVsMatch {
-  val Iterations = 1000
+// @Warmup(iterations = 2)
+// @Measurement(iterations = 10)
+// @State(Scope.Benchmark)
+// class VisitorVsMatch {
+//   val Iterations = 1000
 
-  // @Benchmark
-  // @BenchmarkMode(Array(Mode.Throughput))
-  // @Fork(value = 1)
-  // def matcher2: Unit = {
-  //   val iter = infiniteNodeIterator(2)
-  //   1.to(Iterations).foreach(_ =>
-  //     Matcher2.foo(iter.next)
-  //   )
-  // }
+//   // @Benchmark
+//   // @BenchmarkMode(Array(Mode.Throughput))
+//   // @Fork(value = 1)
+//   // def matcher2: Unit = {
+//   //   val iter = infiniteNodeIterator(2)
+//   //   1.to(Iterations).foreach(_ =>
+//   //     Matcher2.foo(iter.next)
+//   //   )
+//   // }
 
-  @Benchmark
-  @BenchmarkMode(Array(Mode.Throughput))
-  @Fork(value = 1)
-  def visitor1: Unit = {
-    val iter = infiniteNodeIterator(1)
-    1.to(Iterations).foreach(_ =>
-      iter.next.accept(Visitor1)
-    )
-  }
+//   @Benchmark
+//   @BenchmarkMode(Array(Mode.Throughput))
+//   @Fork(value = 1)
+//   def visitor1: Unit = {
+//     val iter = infiniteNodeIterator(1)
+//     1.to(Iterations).foreach(_ =>
+//       iter.next.accept(Visitor1)
+//     )
+//   }
 
-  @Benchmark
-  @BenchmarkMode(Array(Mode.Throughput))
-  @Fork(value = 1)
-  def visitor2: Unit = {
-    val iter = infiniteNodeIterator(2)
-    1.to(Iterations).foreach(_ =>
-      iter.next.accept(Visitor2)
-    )
-  }
+//   @Benchmark
+//   @BenchmarkMode(Array(Mode.Throughput))
+//   @Fork(value = 1)
+//   def visitor2: Unit = {
+//     val iter = infiniteNodeIterator(2)
+//     1.to(Iterations).foreach(_ =>
+//       iter.next.accept(Visitor2)
+//     )
+//   }
 
-  // @Benchmark
-  // @BenchmarkMode(Array(Mode.Throughput))
-  // @Fork(value = 1)
-  // def matcher3: Unit = {
-  //   val iter = infiniteNodeIterator(3)
-  //   1.to(Iterations).foreach(_ =>
-  //     Matcher3.foo(iter.next)
-  //   )
-  // }
+//   // @Benchmark
+//   // @BenchmarkMode(Array(Mode.Throughput))
+//   // @Fork(value = 1)
+//   // def matcher3: Unit = {
+//   //   val iter = infiniteNodeIterator(3)
+//   //   1.to(Iterations).foreach(_ =>
+//   //     Matcher3.foo(iter.next)
+//   //   )
+//   // }
 
-  @Benchmark
-  @BenchmarkMode(Array(Mode.Throughput))
-  @Fork(value = 1)
-  def visitor3: Unit = {
-    val iter = infiniteNodeIterator(3)
-    1.to(Iterations).foreach(_ =>
-      iter.next.accept(Visitor3)
-    )
-  }
+//   @Benchmark
+//   @BenchmarkMode(Array(Mode.Throughput))
+//   @Fork(value = 1)
+//   def visitor3: Unit = {
+//     val iter = infiniteNodeIterator(3)
+//     1.to(Iterations).foreach(_ =>
+//       iter.next.accept(Visitor3)
+//     )
+//   }
 
-  // @Benchmark
-  // @BenchmarkMode(Array(Mode.Throughput))
-  // @Fork(value = 1)
-  // def matcher5: Unit = {
-  //   val iter = infiniteNodeIterator(5)
-  //   1.to(Iterations).foreach(_ =>
-  //     Matcher5.foo(iter.next)
-  //   )
-  // }
+//   // @Benchmark
+//   // @BenchmarkMode(Array(Mode.Throughput))
+//   // @Fork(value = 1)
+//   // def matcher5: Unit = {
+//   //   val iter = infiniteNodeIterator(5)
+//   //   1.to(Iterations).foreach(_ =>
+//   //     Matcher5.foo(iter.next)
+//   //   )
+//   // }
 
-  // @Benchmark
-  // @BenchmarkMode(Array(Mode.Throughput))
-  // @Fork(value = 1)
-  // def visitor5: Unit = {
-  //   val iter = infiniteNodeIterator(5)
-  //   1.to(Iterations).foreach(_ =>
-  //     iter.next.accept(Visitor5)
-  //   )
-  // }
+//   // @Benchmark
+//   // @BenchmarkMode(Array(Mode.Throughput))
+//   // @Fork(value = 1)
+//   // def visitor5: Unit = {
+//   //   val iter = infiniteNodeIterator(5)
+//   //   1.to(Iterations).foreach(_ =>
+//   //     iter.next.accept(Visitor5)
+//   //   )
+//   // }
 
-  // @Benchmark
-  // @BenchmarkMode(Array(Mode.Throughput))
-  // @Fork(value = 1)
-  // def matcher10: Unit = {
-  //   val iter = infiniteNodeIterator(10)
-  //   1.to(Iterations).foreach(_ =>
-  //     Matcher10.foo(iter.next)
-  //   )
-  // }
+//   // @Benchmark
+//   // @BenchmarkMode(Array(Mode.Throughput))
+//   // @Fork(value = 1)
+//   // def matcher10: Unit = {
+//   //   val iter = infiniteNodeIterator(10)
+//   //   1.to(Iterations).foreach(_ =>
+//   //     Matcher10.foo(iter.next)
+//   //   )
+//   // }
 
-  // @Benchmark
-  // @BenchmarkMode(Array(Mode.Throughput))
-  // @Fork(value = 1)
-  // def visitor10: Unit = {
-  //   val iter = infiniteNodeIterator(10)
-  //   1.to(Iterations).foreach(_ =>
-  //     iter.next.accept(Visitor10)
-  //   )
-  // }
+//   // @Benchmark
+//   // @BenchmarkMode(Array(Mode.Throughput))
+//   // @Fork(value = 1)
+//   // def visitor10: Unit = {
+//   //   val iter = infiniteNodeIterator(10)
+//   //   1.to(Iterations).foreach(_ =>
+//   //     iter.next.accept(Visitor10)
+//   //   )
+//   // }
 
-  // @Benchmark
-  // @BenchmarkMode(Array(Mode.Throughput))
-  // @Fork(value = 1)
-  // def matcher20: Unit = {
-  //   val iter = infiniteNodeIterator(20)
-  //   1.to(Iterations).foreach(_ =>
-  //     Matcher20.foo(iter.next)
-  //   )
-  // }
+//   // @Benchmark
+//   // @BenchmarkMode(Array(Mode.Throughput))
+//   // @Fork(value = 1)
+//   // def matcher20: Unit = {
+//   //   val iter = infiniteNodeIterator(20)
+//   //   1.to(Iterations).foreach(_ =>
+//   //     Matcher20.foo(iter.next)
+//   //   )
+//   // }
 
-  // @Benchmark
-  // @BenchmarkMode(Array(Mode.Throughput))
-  // @Fork(value = 1)
-  // def visitor20: Unit = {
-  //   val iter = infiniteNodeIterator(20)
-  //   1.to(Iterations).foreach(_ =>
-  //     iter.next.accept(Visitor20)
-  //   )
-  // }
+//   // @Benchmark
+//   // @BenchmarkMode(Array(Mode.Throughput))
+//   // @Fork(value = 1)
+//   // def visitor20: Unit = {
+//   //   val iter = infiniteNodeIterator(20)
+//   //   1.to(Iterations).foreach(_ =>
+//   //     iter.next.accept(Visitor20)
+//   //   )
+//   // }
 
-  def infiniteNodeIterator(nodeCount: Int) = new java.util.Iterator[Node] {
-    val nodes: Array[Node] = Array(
-      new N1,
-      new N2,
-      new N3,
-      new N4,
-      new N5,
-      new N6,
-      new N7,
-      new N8,
-      new N9,
-      new N10,
-      new N11,
-      new N12,
-      new N13,
-      new N14,
-      new N15,
-      new N16,
-      new N17,
-      new N18,
-      new N19,
-      new N20
-    )
-    var i = 0
-    def hasNext(): Boolean = true
-    def next(): Node = {
-      val node = nodes(i)
-      i += 1
-      if (i == nodeCount) i = 0
-      node
-    }
-  }
-}
-
-// object VisitorApp extends App {
-//   val v = new VisitorVsMatch
-
-//   var i = 0
-//   while (i < 10000000) {
-//     if (i % 100000 == 0) println(i)
-//     v.visitor20
-//     i += 1
+//   def infiniteNodeIterator(nodeCount: Int) = new java.util.Iterator[Node] {
+//     val nodes: Array[Node] = Array(
+//       new N1,
+//       new N2,
+//       new N3,
+//       new N4,
+//       new N5,
+//       new N6,
+//       new N7,
+//       new N8,
+//       new N9,
+//       new N10,
+//       new N11,
+//       new N12,
+//       new N13,
+//       new N14,
+//       new N15,
+//       new N16,
+//       new N17,
+//       new N18,
+//       new N19,
+//       new N20
+//     )
+//     var i = 0
+//     def hasNext(): Boolean = true
+//     def next(): Node = {
+//       val node = nodes(i)
+//       i += 1
+//       if (i == nodeCount) i = 0
+//       node
+//     }
 //   }
 // }
 
-// object MatcherApp extends App {
-//   val v = new VisitorVsMatch
-
-//   var i = 0
-//   while (i < 10000000) {
-//     if (i % 100000 == 0) println(i)
-//     v.matcher20
-//     i += 1
-//   }
-// }
