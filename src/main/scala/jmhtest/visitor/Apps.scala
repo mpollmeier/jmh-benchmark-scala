604ffb4947782486c5a920f93b664b476aed70da
Michael Pollmeier <michael@michaelpollmeier.com>
2019-11-13 22:04:53 +1300

    apps

---
+++ src/main/scala/jmhtest/visitor/Apps.scala
@@ -0,0 +1,30 @@
+package jmhtest.visitor
+
+object Visitor2App extends App {
+  var i = 0
+  while (i < 1000000000) {
+    VisitorJmh.iter2.next.accept(Visitor2)
+    // if (i % 1000000 == 0) println(i)
+    i += 1
+  }
+}
+
+object Visitor3App extends App {
+  var i = 0
+  while (i < 1000000000) {
+    VisitorJmh.iter3.next.accept(Visitor3)
+    // if (i % 1000000 == 0) println(i)
+    i += 1
+  }
+}
+
+// object MatcherApp extends App {
+//   val v = new VisitorVsMatch
+
+//   var i = 0
+//   while (i < 10000000) {
+//     if (i % 100000 == 0) println(i)
+//     v.matcher20
+//     i += 1
+//   }
+// }
