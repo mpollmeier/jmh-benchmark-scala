dd50f7b08eaac3fe15411787b195caf6887cd48e
Michael Pollmeier <michael@michaelpollmeier.com>
2019-11-15 14:50:24 +1300

    trying out blackhole

--- src/main/scala/jmhtest/visitor/Apps.scala
+++ src/main/scala/jmhtest/visitor/Apps.scala
@@ -1,22 +1,22 @@
 package jmhtest.visitor
 
-object Visitor2App extends App {
-  var i = 0
-  while (i < 1000000000) {
-    VisitorJmh.iter2.next.accept(Visitor2)
-    // if (i % 1000000 == 0) println(i)
-    i += 1
-  }
-}
+// object Visitor2App extends App {
+//   var i = 0
+//   while (i < 1000000000) {
+//     VisitorJmh.iter2.next.accept(Visitor2)
+//     // if (i % 1000000 == 0) println(i)
+//     i += 1
+//   }
+// }
 
-object Visitor3App extends App {
-  var i = 0
-  while (i < 1000000000) {
-    VisitorJmh.iter3.next.accept(Visitor3)
-    // if (i % 1000000 == 0) println(i)
-    i += 1
-  }
-}
+// object Visitor3App extends App {
+//   var i = 0
+//   while (i < 1000000000) {
+//     VisitorJmh.iter3.next.accept(Visitor3)
+//     // if (i % 1000000 == 0) println(i)
+//     i += 1
+//   }
+// }
 
 // object MatcherApp extends App {
 //   val v = new VisitorVsMatch
--- src/main/scala/jmhtest/visitor/VisitorJmh.scala
+++ src/main/scala/jmhtest/visitor/VisitorJmh.scala
@@ -1,6 +1,7 @@
 package jmhtest.visitor
 
 import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.infra.Blackhole
 
 object VisitorJmh {
   val iter2 = infiniteNodeIterator(new N1, new N2)
@@ -19,6 +20,7 @@ object VisitorJmh {
       node
     }
   }
+
 }
 
 // @Warmup(iterations = 20)
@@ -26,36 +28,91 @@ object VisitorJmh {
 // @Warmup(iterations = 2)
 // @Measurement(iterations = 10)
 @Warmup(iterations = 1)
-@Measurement(iterations = 2)
+@Measurement(iterations = 5)
 @State(Scope.Benchmark)
 class VisitorJmh {
   import VisitorJmh._
-  val Iterations = 1000
+  val Iterations = 1000000
+
+  // blackhole a la http://hg.openjdk.java.net/code-tools/jmh/file/c8f9f5b85cd9/jmh-samples/src/main/java/org/openjdk/jmh/samples/JMHSample_28_BlackholeHelpers.java
+  trait Worker {
+    def work(): Unit
+  }
+  var workerBaseline: Worker = null
+  var workerWrong: Worker = null
+  var workerRight: Worker = null
+  var blackhole: Blackhole = null
+
+  @Setup
+  def setup(blackhole: Blackhole): Unit = {
+    this.blackhole = blackhole
+    workerBaseline = new Worker {
+      final override def work(): Unit = {
+        // do nothing
+      }
+    }
+    workerWrong = new Worker {
+      val x = 0d
+      final override def work(): Unit = {
+        Math.log(x)
+      }
+    }
+    workerRight = new Worker {
+      val x = 0d
+      final override def work(): Unit = {
+        blackhole.consume(Math.log(x))
+      }
+    }
+    println("setup called! xxxxxxxxxx")
+  }
+
 
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
-  @Fork(value = 1)
-  // @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+LogCompilation", "-XX:+PrintAssembly" ,"-XX:+PrintInterpreter", "-XX:+PrintNMethods", "-XX:+PrintNativeNMethods", "-XX:+PrintSignatureHandlers", "-XX:+PrintAdapterHandlers", "-XX:+PrintStubCode", "-XX:+PrintCompilation", "-XX:+PrintInlining", "-XX:CompileThreshold=1000"))
+  // @Fork(value = 1)
+  @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+PrintCompilation", "-XX:+PrintInlining", "-XX:CompileThreshold=1000"))
   def visitor2: Unit = {
     var i = 0
     while (i < Iterations) {
       i += 1
       iter2.next.accept(Visitor2)
     }
-    println(s"visitor2: $i")
   }
 
   @Benchmark
   @BenchmarkMode(Array(Mode.Throughput))
-  @Fork(value = 1)
-  // @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+LogCompilation", "-XX:+PrintAssembly" ,"-XX:+PrintInterpreter", "-XX:+PrintNMethods", "-XX:+PrintNativeNMethods", "-XX:+PrintSignatureHandlers", "-XX:+PrintAdapterHandlers", "-XX:+PrintStubCode", "-XX:+PrintCompilation", "-XX:+PrintInlining", "-XX:CompileThreshold=1000"))
+  // @Fork(value = 1)
+  @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+PrintCompilation", "-XX:+PrintInlining", "-XX:CompileThreshold=1000"))
+  def visitor2WithBlackhole: Unit = {
+    var i = 0
+    while (i < Iterations) {
+      i += 1
+      blackhole.consume(iter2.next.accept(Visitor2))
+    }
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.Throughput))
+  // @Fork(value = 1)
+  @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+PrintCompilation", "-XX:+PrintInlining", "-XX:CompileThreshold=1000"))
   def visitor3: Unit = {
     var i = 0
     while (i < Iterations) {
       i += 1
       iter3.next.accept(Visitor3)
     }
-    println(s"visitor3: $i")
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.Throughput))
+  // @Fork(value = 1)
+  @Fork(value = 1, jvmArgsAppend = Array("-XX:+UnlockDiagnosticVMOptions", "-XX:+PrintCompilation", "-XX:+PrintInlining", "-XX:CompileThreshold=1000"))
+  def visitor3WithBlackhole: Unit = {
+    var i = 0
+    while (i < Iterations) {
+      i += 1
+      blackhole.consume(iter3.next.accept(Visitor3))
+    }
   }
 
 
